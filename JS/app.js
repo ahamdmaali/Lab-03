a40e0985d7f54133fb07885aee04a444e0db2888
ahamdmaali <ahmad.maali95@outlook.com>
2021-02-25 11:45:11 +0200

    edit JS file

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,40 @@
 var userName = confirm("Are you like our products?");
-var amountofcups = prompt("how many cups you need?");
+var a = prompt("What do want to drink?");
+var i = 0;
 
-function myfunction(amountofcups) {
-    var totalprice = amountofcups*2.5 +"JD";
-    return totalprice;
+while (a[i]){
+if (a === "tea"){
+    document.write('<img id= :tea" src="https://www.omechaye.com/static/sitefiles/tea_at_omechaye.jpg" alt="Tea">')
+}else if (a === "nescafe"){
+    document.write('<img id= nescafe src="https://www.andreashop.sk/files/product/money/STARBUCKS_NESCAFE_DOLCE_GUSTO_CARAMEL_MACCHIATO_12_KAPSUL_6.jpg_OID_6VM9200101.jpg" alt="Nescafe">')
+} else if (a === "hot choco"){
+    document.write('<img id="hotchoc" src="https://shop-academyart.sodexomyway.com/content/images/thumbs/0012566_beverages-starbucks-instant-hot-cocoa-125-oz-packet.jpeg" alt="Hot Choca">')
+}else if (a === "coffee"){
+    document.write('<img id="hotchoc" src="https://th.bing.com/th/id/OIP.XGODvBoD9ihbWA3AQPEb6QHaHk?pid=ImgDet&rs=1" alt="Hot Choca">')
 }
-myfunction(amountofcups);
-alert (myfunction(amountofcups));
+    i++;
+    }
+
+    
+
+    
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<img id="logo" src="https://th.bing.com/th/id/OIP.tywgPzoDQkLaYLZoK2RMzgHaHE?pid=ImgDet&rs=1" alt="Starbucks"> 
+   // <img id= :tea" src="https://www.omechaye.com/static/sitefiles/tea_at_omechaye.jpg" alt="Tea">
+    //<img id= nescafe src="https://www.andreashop.sk/files/product/money/STARBUCKS_NESCAFE_DOLCE_GUSTO_CARAMEL_MACCHIATO_12_KAPSUL_6.jpg_OID_6VM9200101.jpg" alt="Nescafe">
+    //<img id="hotchoc" src="https://shop-academyart.sodexomyway.com/content/images/thumbs/0012566_beverages-starbucks-instant-hot-cocoa-125-oz-packet.jpeg" alt="Hot Choca">
